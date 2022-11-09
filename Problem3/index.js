16d452f3e0ab527170539f0ae1b7a129c24361c1
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2022-11-09 13:35:12 -0600

    Add files via upload

--- Problem3/index.js
+++ Problem3/index.js
@@ -1,21 +1,12 @@
-function change(e,color="blue",fontsize="1.5rem",background="#979A9A"){
-    if(this!=window){ 
-        this.style.color= color;
-        this.style.fontSize = fontsize;
-        this.style.backgroundColor= background;
-    }else{
-        let el = document.getElementById(e.id)
-        el.style.color= color;
-        el.style.fontSize = fontsize;
-        el.style.backgroundColor= background;
-    }
-
-}
-
-function specificchange(){
-    return change(this, 'yellow','16px','blue')
+function change(color="blue",fontsize="1.5rem",background="#979A9A"){
+    this.style.color= color;
+    this.style.fontSize = fontsize;
+    this.style.backgroundColor= background;
 }
 
 let el= document.getElementById("example")
-el.onclick=specificchange;
-//i dont know if this is a problem, but the two cases work, the difference is what function needs to be called in order to use the attribute values
+let changer = change.bind(el);
+changer('orange', '2rem','red')//use this line to test with given arguments
+//changer() //Use this line to test the default values
+//Methods like call(), apply(), and bind() can refer this to any object.
+//the event handler proved to be more tricky, so binding the context was easier for the sake of the problem
